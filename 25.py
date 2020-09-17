5c116a7ba19b61b963e34e62699c40bb596f0dac
ali-04 <mahmohamad560@gmail.com>
2020-09-17 12:11:43 +0430

    Update 24.py

---
+++ 25.py
@@ -0,0 +1,21 @@
+e = int(input ())
+w = list()
+from math import sqrt
+
+
+for a in range (0,e):
+    w.append('')
+    w [a] =  int (input())
+
+
+for a in range (0,e):
+    print (sqrt( w[a]))
+
+
+
+
+
+
+
+
+
