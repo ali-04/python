f4a455a3fb16e142fef300746e082c9700d5e177
ali-04 <mahmohamad560@gmail.com>
2020-09-17 12:11:43 +0430

    Update 24.py

--- 24.py
+++ 24.py
@@ -1,10 +1,23 @@
-a = input()
+a = int(input())
+f = dict()
+f1 = list()
+ae = str()
+
+for g in range (0,a):
+    f1 = input ().split()
+    f[f1[0]] = f1[1]
+    ae += f1[0] + '   '
 
 
+t = input().split()
 
+for n in range (0,len(t)) :
+    if t[n] in ae :
+        t[n] = f [t[n]]
+    
+j =' '.join(t)
 
-
-
+print(j)
 
 
 
