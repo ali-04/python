0b544cede914cc2f286a9e1029a4e46f802f05f0
ali-04 <mahmohamad560@gmail.com>
2020-09-19 16:41:29 +0430

    update a hard project

--- source.py
+++ source.py
@@ -2,8 +2,9 @@ from csv import reader
 #from Collections import OrderedDict
 from statistics import mean 
 from os import getcwd
-#calculate_averages
-def W (input_file_name, output_file_name):
+#print (getcwd()+"\\"[0]+"1.csv",getcwd()+"\\"[0]+"0.csv")
+
+def calculate_averages (input_file_name, output_file_name):
     e = list()
     r = open (output_file_name,"w")
     with open (input_file_name) as f :
@@ -26,7 +27,41 @@ def W (input_file_name, output_file_name):
     
 
 
-def calculate_sorted_averages(input_file_name, output_file_name):
+def calculate_sorted_averages (input_file_name, output_file_name):
+    r = open (output_file_name,"w")
+    m = dict()
+    cc = list()
+    with open (input_file_name) as f :
+        d = reader(f)
+        po = list(d)
+        oio = int(len(po))
+        for aa in po:
+            for ds in range (1,len(list(aa))) :
+                aa [ds] = int(aa [ds])
+            m [aa[0]]=mean(aa[1:])
+            cc.append (aa[0])
+        for lk in range (0,oio):
+            for lm in range (0,oio):
+                if m[cc[lm]] > m[cc[lk]] :
+                    nnnn = cc[lm]
+                    cc[lm] = cc[lk]
+                    cc[lk] = nnnn
+                elif cc[lm] > cc[lk] and m[cc[lm]] == m[cc[lk]]:
+                    nnnn = cc[lm]
+                    cc[lm] = cc[lk]
+                    cc[lk] = nnnn
+        for ol in cc :
+            sss = (str(ol)+","+str(m[ol]))
+            r.write (sss+'''
+''')
+    r.close ()
+
+
+
+            
+
+
+
     
     
 
@@ -41,7 +76,7 @@ def calculate_sorted_averages(input_file_name, output_file_name):
 
 #def calculate_average_of_averages(input_file_name, output_file_name):
     
-#W  (getcwd()+"\\"[0]+"1.csv",getcwd()+"\\"[0]+"0.csv")
+#calculate_sorted_averages  (str(getcwd()+"\\"[0]+"1.csv"),str(getcwd()+"\\"[0]+"0.csv"))
 
 
 
