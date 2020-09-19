4e966b24dfece77bb7cf21333f5ff4bb825c75fd
ali-04 <mahmohamad560@gmail.com>
2020-09-19 06:57:27 +0430

    add location and "csv" reading

---
+++ 26.py
@@ -0,0 +1,32 @@
+from os import getcwd 
+from csv import reader 
+
+dd = '\\'
+location = dd [0].join((getcwd() + dd[0] +"1.csv").split()) 
+print (location)
+
+with open (location) as f :
+    print (type (f))
+    data = list(reader(f))
+    print (data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
