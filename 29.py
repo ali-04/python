f1ef0c03be8ef302c07e3a764719060898672524
ali-04 <mahmohamad560@gmail.com>
2020-09-24 12:09:08 +0330

    update 29

--- 29.py
+++ 29.py
@@ -9,6 +9,10 @@ for j in range (0,10):
     if z < sn :
         z = sn
     
+for d in s :
+    if d < 2 :
+        s.remove(d)
+
 
 for k in range (2,z+1):
     for t in range (2,k+1):
@@ -18,15 +22,22 @@ for k in range (2,z+1):
         cc.append (k)
     x = 0
 
+z = 0
+x = 0
+u = 0
+#print (cc)
 
+for ra in s :
+    for ma in cc :
+        if ra % cc == 0 :
+            x +=1
+    if x > z :
+        z = x
+        u = ra 
+    elif x = z and ra > u :
+        u = ra 
 
-
-print (cc)
-
-
-
-
-
+print (u,z)
 
 
 
