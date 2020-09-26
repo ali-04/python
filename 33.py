25838e364e13de7a5bde15975ab318624dc5eaa7
ali-04 <mahmohamad560@gmail.com>
2020-09-26 16:32:02 +0330

    Debug 33

---
+++ 33.py
@@ -0,0 +1,26 @@
+j = list()
+qqq = input()
+yj = qqq.split('.')
+er = qqq.split()
+ww = {0: 0}
+for vv in range(0,len(yj)):
+    s = yj[vv].split()
+    if vv != 0:
+        ww [vv] = len(zx)
+    for a in range (1,len(s)):
+        f = s[a]
+        if f[0] != f[0].lower():
+            j.append(a+ww[vv])
+
+    zx = s
+
+for gg in range(0,len(j)):
+    zzz = j[gg]
+    eee = str(zzz+1)+':'+er[zzz]
+    if eee.find('.') == -1:
+        print (eee)
+    else :
+        eee = eee[:len(eee)-1]
+        print (eee)
+
+
