c9281951b6dfb0a6b2aa431cf95e5c5d69107b2c
ali-04 <mahmohamad560@gmail.com>
2020-10-01 10:14:08 +0330

    do first part at 35

--- 35.py
+++ 35.py
@@ -1,6 +1,21 @@
+a = list ()
+b = list ()
+u = ['age' ,'ghad' ,'vazn']
 
+def inp(x):
+    o = int(input())
+    for q in range(0,o):
+        x.append(dict())
+        iii = q
 
+    
+    for ww in u:
+        zzz = input().split()
+        for xx in range(0,o):
+            x [xx] [ww] = int(zzz[xx])
 
+inp (b)
+inp (a)
 
 
 
