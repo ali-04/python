078c8209f6a3c0a9153fcd73f6b4e8fa1e116fa3
ali-04 <mahmohamad560@gmail.com>
2020-10-03 11:27:17 +0330

    end 35

--- 35.py
+++ 35.py
@@ -5,10 +5,20 @@ class student:
         self.ghad = int(y)
         self.age = int(x)
         self.vazn = int(z)
-        
-
-
     
+def mean1 (cc,a,b):
+    rq = list()
+    for yyy in range (0,b):
+        if a == 'age' :
+            rte = cc[yyy].age
+        elif a == 'ghad':
+            rte = cc[yyy].ghad
+        else :
+            rte = cc[yyy].vazn
+        rq.append(rte)
+    return float(mean(rq))
+        
+        
 
 
 fa = list ()
@@ -20,6 +30,7 @@ u = ['age' ,'ghad' ,'vazn']
 
 def inp(x,xx):
     o = int(input())
+    x.append(o)
     for q in range(0,o):
         x.append(dict())
         iii = q
@@ -27,21 +38,45 @@ def inp(x,xx):
     
     for ww in u:
         zzz = input().split()
-        for asd in range(0,o):
-            x [asd] [ww] = int(zzz[asd])
+        for asd in range(1,o+1):
+            x [asd] [ww] = int(zzz[asd-1])
     
-    for ooo in range(0,len(x)) :
+    for ooo in range(1,len(x)) :
         xx.append ( student(x[ooo]['age'],x[ooo]['ghad'],x[ooo]['vazn'])  )
+    
+    
+
+    
 
 
-inp (b,bb)
+
 inp (a,aa)
+inp (b,bb)
 
 
+print (mean1(aa,'age',a[0]))
+print (mean1(aa,'ghad',a[0]))
+print (mean1(aa,'vazn',a[0]))
+print (mean1(bb,'age',b[0]))
+print (mean1(bb,'ghad',b[0]))
+print (mean1(bb,'vazn',b[0]))
+
+if mean1(aa,'ghad',a[0]) != mean1(bb,'ghad',b[0]):
+    if mean1(aa,'ghad',a[0]) < mean1(bb,'ghad',b[0]):
+        drtd = 'B'
+    else:
+        drtd = 'A'
+elif mean1(aa,'vazn',a[0]) != mean1(bb,'vazn',b[0]):
+    if mean1(aa,'vazn',a[0]) > mean1(bb,'vazn',b[0]):
+        drtd = 'B'
+    else:
+        drtd = 'A'
+elif (mean1(aa,'ghad',a[0]) == mean1(bb,'ghad',b[0])) and (mean1(aa,'vazn',a[0]) == mean1(bb,'vazn',b[0])):
+    drtd = 'Same'
+
+print(drtd)
 
 
-
-
-
+a = 1
 
 
