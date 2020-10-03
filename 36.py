2695316484285d248244549f8fdba63d63c88511
ali-04 <mahmohamad560@gmail.com>
2020-10-03 14:52:59 +0330

    add option 36

--- 36.py
+++ 36.py
@@ -1,14 +1,21 @@
 import datetime
-q = ('-'.join(input().split('/')))
+u = input ()
+ccc=[]
+pq = u.split('/')
+for xxx in pq :
+    ccc.append(int(xxx))
+pq = ccc
+if len(str(pq [0]))!= 4 or len (pq) != 3 or len(str(pq [1]))!= 2  or pq [2] > 31 or pq [2] < 1 or pq [1] > 12 or pq [1] < 1 :
+    print('WRONG')
+else:
+    q = ('-'.join(u.split('/')))
 
-x = datetime.date.fromisoformat(q)
-y = datetime.date.today()
+    x = datetime.date.fromisoformat(q)
+    y = datetime.date.today()
 
-e = y-x
+    e = y-x
 
-print  (int(e.days)//365)
-
-
+    print  (int(e.days)//365)
 
 
 
@@ -18,4 +25,6 @@ print  (int(e.days)//365)
 
 
 
+
+ 
 
