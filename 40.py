aac852f6ae0fa7417035d08ebc0c770e69f56ab4
ali-04 <mahmohamad560@gmail.com>
2020-11-12 13:17:12 -0500

    add 41

--- 40.py
+++ 40.py
@@ -1,12 +1,27 @@
+
 from re import findall 
 w = input()
+
+
+
+def list_get (lis,x,y):
+    if len(lis) > x :
+        return lis[x]
+    else:
+        return y
+
+
+
+
 def in1 (x):
     return findall("^[\w\d]+@\w+\.[\w]+$",x)
 
-if findall(".+@[^\d]+\.[^\d]+",in1(w)[0]) == []:
+
+
+if findall(".+@[^\d]+\.[^\d]+",list_get(in1(w),0,'')) == []:
     print ('WRONG')
 else:
-    if findall('[^\d]',findall ('^(.+)@',w)[0]) != []:
+    if findall('[^\d]',list_get(findall ('^(.+)@',w),0,'')) != []:
         print('OK')
     else:
         print ('WRONG')
---
+++ 41.py
@@ -0,0 +1,32 @@
+
+from bs4 import BeautifulSoup
+
+from requests import get
+
+e = get('https://divar.ir/s/tehran')
+
+
+soup = BeautifulSoup(e.text,'html.parser')
+  
+  
+val = soup.findAll('a')
+
+c = 0
+
+for q in val[36:] :
+    ee = q.text
+    if 'توافقی' in ee:
+        print ('\n\n\n\n%i:\n%s'%(c,ee))
+        c+=1
+
+
+
+
+
+
+
+
+
+
+
+
