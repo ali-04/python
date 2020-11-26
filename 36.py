49c6e863d987717b03d902330b37734d72b93b79
ali-04 <mahmohamad560@gmail.com>
2020-11-26 20:00:06 +0330

    a good program

--- 36.py
+++ 36.py
@@ -1,21 +1,56 @@
 import datetime
+from re import findall as find
+
+def WRONG ():
+    print ('WRONG')
+    exit()
+
 u = input ()
+def b_ad (ad):
+    if find(r"(\d+)",ad)==[]:
+        WRONG()
+        return False
+    elif find(r"(\d+)",ad)[0] != ad :
+        WRONG()
+        return False
+    else :
+        return True 
+
+
 ccc=[]
 pq = u.split('/')
 for xxx in pq :
-    ccc.append(int(xxx))
+    if b_ad(xxx):
+        ccc.append(int(xxx))
+
 pq = ccc
-if len(str(pq [0]))!= 4 or len (pq) != 3 or len(str(pq [1]))!= 2  or pq [2] > 31 or pq [2] < 1 or pq [1] > 12 or pq [1] < 1 :
-    print('WRONG')
+
+
+if  find(r'(\d{4}/\d{1,2}/\d{1,2})',u) == None :
+    WRONG()
+elif find(r'(\d{4}/\d{1,2}/\d{1,2})',u) [0] != u:
+    WRONG()
+
+
+
+elif  pq [2] > 31 or pq [2] < 1 or pq [1] > 12 or pq [1] < 1 :
+    WRONG()
 else:
     q = ('-'.join(u.split('/')))
 
     x = datetime.date.fromisoformat(q)
     y = datetime.date.today()
+    if x > y :
+        WRONG()
+    else:
+    
+        e = y-x
 
-    e = y-x
+        frt = int (e.days) * 24 * 60 * 60
 
-    print  (int(e.days)//365)
+
+        print  (frt // 31557600)
+
 
 
 
